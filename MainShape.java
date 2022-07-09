171b28e438aacd29804aa2ac9fafb4a0e61af16a
Mohit Mathur <71831549+mohitmathurtech@users.noreply.github.com>
2022-07-09 22:56:08 +0530

    Adding new implementations of OOPs

--- MainShape.java
+++ MainShape.java
@@ -5,8 +5,10 @@ public class MainShape {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         Circle circle = new Circle("Red" , 14);
+        Rectangle rectangle = new Rectangle("Blue",10,4);
         
         System.out.println("Area of cirle " + circle.calculateArea());
-	}
-
+        System.out.println("Perimeter of cirle " + circle.calculatePerimeter());
+        System.out.println("Area of Rectangle is " + rectangle.calculateArea());
+        System.out.println("Perimeter of Rectangle" + rectangle.calculatePerimeter());
 }
